f94ac4a83c9527b7933b20712c620a088598d367
James Briggs <jimy.pbr@gmail.com>
2014-12-05 01:36:53 +0000

    adding cmake build with unix makefile

---
+++ build/CMakeFiles/hex.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/user/programming/cpp4c/hex/aiPlayer.cpp" "/Users/user/programming/cpp4c/hex/build/CMakeFiles/hex.dir/aiPlayer.cpp.o"
+  "/Users/user/programming/cpp4c/hex/cli_hex.cpp" "/Users/user/programming/cpp4c/hex/build/CMakeFiles/hex.dir/cli_hex.cpp.o"
+  "/Users/user/programming/cpp4c/hex/game.cpp" "/Users/user/programming/cpp4c/hex/build/CMakeFiles/hex.dir/game.cpp.o"
+  "/Users/user/programming/cpp4c/hex/hexGraph.cpp" "/Users/user/programming/cpp4c/hex/build/CMakeFiles/hex.dir/hexGraph.cpp.o"
+  "/Users/user/programming/cpp4c/hex/main.cpp" "/Users/user/programming/cpp4c/hex/build/CMakeFiles/hex.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
