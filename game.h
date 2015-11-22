b9d9c60ba8a685d8b0d8aa0c7b155fec49480aa8
James Briggs <jimy.pbr@gmail.com>
2015-11-22 21:32:50 +0000

    removing ntiles_ and side_ private member variables for the game class. This information can be queried from the board object.

--- game.h
+++ game.h
@@ -3,8 +3,6 @@
 #include <memory>
 #include "board.h"
 #include "hexGraph.h"
-#include "aiPlayer.h"
-#include "mcSearchTreePlayer.h"
 #include "player.h"
 #include "humanPlayerCLI.h"
 #include "computerPlayer.h"
@@ -12,12 +10,6 @@
 class HexGame
 {
 	private:
-        /* The number of tiles on the game board side */
-		int side_;
-
-        /* The number of tiles, which is the side_**2 */
-		int ntiles_;
-
         /* The board representing the current state of play. */
 		Board main_board_;
 		std::unique_ptr<Player> p1_;
@@ -32,13 +24,13 @@ class HexGame
          * Creates an instance of HexGame with a board of side s.
          * @param s: size of the side of the Hex board.
          */
-		HexGame(int s) : side_(s), ntiles_(s*s), main_board_(s)
+		HexGame(int s) : main_board_(s)
         {
 			p1_ = std::unique_ptr<Player>(new HumanPlayerCLI(p1_colour_));
             p2_ = std::unique_ptr<Player>(new ComputerPlayer(p2_colour_, AIStrategyEnum::MCTS, true));
         }
 
-        HexGame(int s, std::unique_ptr<Player> p1, std::unique_ptr<Player> p2) : side_(s), ntiles_(s*s), main_board_(s)
+        HexGame(int s, std::unique_ptr<Player> p1, std::unique_ptr<Player> p2) : main_board_(s)
         {
             p1_ = std::move(p1);
             p2_ = std::move(p2);
